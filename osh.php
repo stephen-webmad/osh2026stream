9fd4414e198d49de4364ae177954897d883c11b3
root <root@stream.webmad.co.nz>
2026-07-21 09:38:53 +1200

    Start day separation hoo-ha - will work out more once we get into the next day

--- osh.php
+++ osh.php
@@ -2,17 +2,20 @@
 
 $d=1;
 if(isset($_GET['d']))$d=$_GET['d'];
+
 $url = "https://fastly.live.brightcove.com/6375380916112/us-west-2/627008079/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJob3N0IjoibDhmNWk3LmVncmVzcy5obnljdWkiLCJhY2NvdW50X2lkIjoiNjI3MDA4MDc5IiwiZWhuIjoiZmFzdGx5LmxpdmUuYnJpZ2h0Y292ZS5jb20iLCJpc3MiOiJibGl2ZS1wbGF5YmFjay1zb3VyY2UtYXBpIiwic3ViIjoicGF0aG1hcHRva2VuIiwiYXVkIjpbIjYyNzAwODA3OSJdLCJqdGkiOiI2Mzc1MzgwOTE2MTEyIn0.cdIoChamG1S-xroVFo-QRPNxWxyISsNYIh-iw_WGXJw/";
 $name = "chunklist_hls1080p.m3u8";
-
 $start = 297425493;
+
+if($d==1){
+    $start = 297425493;
+}
+
 $lastbits = trim(file_get_contents($url.$name));
 $lastnum=array_pop(explode("\n",$lastbits));
 $parts = explode("_",$lastnum);
 $num = explode(".",$parts[2]);
 $end = $num[0];
-// exit;
-
 
 header("Content-type: application/vnd.apple.mpegurl");
 
@@ -21,12 +24,10 @@ echo "#EXTM3U
 #EXT-X-INDEPENDENT-SEGMENTS
 #EXT-X-TARGETDURATION:6
 #EXT-X-MEDIA-SEQUENCE:$start
-#EXT-X-DISCONTINUITY-SEQUENCE:1
-#EXT-X-PROGRAM-DATE-TIME:2026-07-20T".date("H:i:s")."Z";
+#EXT-X-DISCONTINUITY-SEQUENCE:1";
 
 while($end>=$start){
     echo "
-#EXT-X-PROGRAM-DATE-TIME:2026-07-20T18:57:36Z
 #EXTINF:2.0,
 ".$url."media_hls1080p_$start.ts";
     $start++;
