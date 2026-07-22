bc5c9a30e622ac075c64a566d7345f27150d441e
root <root@stream.webmad.co.nz>
2026-07-23 06:35:28 +1200

    day 3 adding

--- osh.php
+++ osh.php
@@ -17,6 +17,17 @@ elseif($d==2){
     $start = 297445554;
     $end = 297461676;
 }
+elseif($d==3){
+    $url = "https://fastly.live.brightcove.com/6401832923112/us-west-2/627008079/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJob3N0IjoibDhmNWk3LmVncmVzcy5obnljdWkiLCJhY2NvdW50X2lkIjoiNjI3MDA4MDc5IiwiZWhuIjoiZmFzdGx5LmxpdmUuYnJpZ2h0Y292ZS5jb20iLCJpc3MiOiJibGl2ZS1wbGF5YmFjay1zb3VyY2UtYXBpIiwic3ViIjoicGF0aG1hcHRva2VuIiwiYXVkIjpbIjYyNzAwODA3OSJdLCJqdGkiOiI2NDAxODMyOTIzMTEyIn0.T13DvhDf9Bzzd1GUzqZYL1dV6TSu4q0rHZvu3dnS-aU/";
+    $start = 297455255;
+//     $end = 297457005;
+    
+    $lastbits = trim(file_get_contents($url.$name));
+    $lastnum=array_pop(explode("\n",$lastbits));
+    $parts = explode("_",$lastnum);
+    $num = explode(".",$parts[2]);
+    $end = $num[0];
+}
 else{
     $start = 297461676;
     
@@ -42,4 +53,4 @@ while($end>=$start){
 ".$url."media_hls1080p_$start.ts";
     $start++;
 }
-if($d==1 || $d==2)echo "\n#EXT-X-ENDLIST";
+if($d==1 || $d==2)echo "\n#EXT-X-ENDLIST";  // || $d==3
