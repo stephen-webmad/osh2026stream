1435cf82aac83f4508ecb2fcf682b9ba11e950ab
root <root@stream.webmad.co.nz>
2026-07-22 15:18:31 +1200

    day 2 added, and instructions on creating video file from teh streams

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Pop file on php ready server, with allow_url_fopen setting enabled.
 Throw url with ?d=1 parameters into vlc player or similar that can play m3u8 files
 Enjoy!
 
+Note -to make your own full video of the streams, use ffmpeg - ie:
+"C:\Program Files (x86)\WinFF\ffmpeg.exe" -i "https://stream.webmad.co.nz/osh/osh.php?d=2" -c copy -bsf:a aac_adtstoasc "osh2026-day2.mp4"
+
 Note - all content is owned by EAA streamed via Brightcove - this code expresses no ownership transfer, it is purely a tool to enable playback outside of Brightcove's player
 We'll try to update this for each day's featured stream at timings that work. Note - we're in New Zealand, so timeframes flexible, but contribute as you like to keep this up to date.
 
--- osh.php
+++ osh.php
@@ -5,17 +5,27 @@ if(isset($_GET['d']))$d=$_GET['d'];
 
 $url = "https://fastly.live.brightcove.com/6375380916112/us-west-2/627008079/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJob3N0IjoibDhmNWk3LmVncmVzcy5obnljdWkiLCJhY2NvdW50X2lkIjoiNjI3MDA4MDc5IiwiZWhuIjoiZmFzdGx5LmxpdmUuYnJpZ2h0Y292ZS5jb20iLCJpc3MiOiJibGl2ZS1wbGF5YmFjay1zb3VyY2UtYXBpIiwic3ViIjoicGF0aG1hcHRva2VuIiwiYXVkIjpbIjYyNzAwODA3OSJdLCJqdGkiOiI2Mzc1MzgwOTE2MTEyIn0.cdIoChamG1S-xroVFo-QRPNxWxyISsNYIh-iw_WGXJw/";
 $name = "chunklist_hls1080p.m3u8";
-$start = 297425493;
+
+$start = 0;
+$end = 0;
 
 if($d==1){
     $start = 297425493;
+    $end = 297445553;
 }
-
-$lastbits = trim(file_get_contents($url.$name));
-$lastnum=array_pop(explode("\n",$lastbits));
-$parts = explode("_",$lastnum);
-$num = explode(".",$parts[2]);
-$end = $num[0];
+elseif($d==2){
+    $start = 297445554;
+    $end = 297461676;
+}
+else{
+    $start = 297461676;
+    
+    $lastbits = trim(file_get_contents($url.$name));
+    $lastnum=array_pop(explode("\n",$lastbits));
+    $parts = explode("_",$lastnum);
+    $num = explode(".",$parts[2]);
+    $end = $num[0];
+}
 
 header("Content-type: application/vnd.apple.mpegurl");
 
@@ -32,3 +42,4 @@ while($end>=$start){
 ".$url."media_hls1080p_$start.ts";
     $start++;
 }
+if($d==1 || $d==2)echo "\n#EXT-X-ENDLIST";
